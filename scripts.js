f7eec9b7776665cd6301a5f02a942b2de2305d4d
Wojciech Szczyrba <szczyrbaw@gmail.com>
2017-11-14 14:18:02 +0100

    portfolio

---
+++ scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() { 
+ 	$("#show").click(function() {
+		
+		$("#nawigacja").show("slow");
+	});
+	$('a[href^="#"]').on('click', function(event) {
+	
+		var target = $( $(this).attr('href') );
+	
+		if( target.length ) {
+			event.preventDefault();
+			$('html, body').animate({
+				scrollTop: target.offset().top
+			}, 700);
+		}
+	});
+	
+	
+});
