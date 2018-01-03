a9e6c9ed60c9bfc6dfa544d7c1fa450986f82703
Wojciech Szczyrba <benice195@gmail.com>
2018-01-03 16:50:52 +0100

    Code review

--- scripts.js
+++ scripts.js
@@ -1,13 +1,26 @@
 $(document).ready(function() { 
- 	$("#show").click(function() {
-		
-		$("#nawigacja").show("slow");
+
+    function showSite() {
+        $(".odslonWolno").show("slow");
+        $(".odslon").show("fast");
+    }
+    if(location.search) {
+        showSite();
+        $('html, body').animate({
+            scrollTop: $("#oMnie").offset().top
+        }, 700);
+    }
+
+    $("#show").click(function() {
+        showSite();
+    });
+		/*$("#nawigacja").show("slow");
 		$("#about").show("fast");
 		$("#techno").show("fast");
 		$("#projekty").show("fast");
 		$("#kontakt").show("fast");
-		$("#stopka").show("fast");
-	});
+		$("#stopka").show("fast");*/
+	
 	$('a[href^="#"]').on('click', function(event) {
 	
 		var target = $( $(this).attr('href') );
